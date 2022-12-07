3f0e75a6cc76b3c082ca63a927af5b8087888d5c
NTDuyQuang <nguyentrandq2999@gmail.com>
2022-12-07 09:17:45 +0700

    note

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+       <p>
+          quang
+       </p>
+       <h1>
+       11111111
+       
+       </h1>
+     
+    </div>
+  )
+}
